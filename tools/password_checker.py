8caef213d101914c6084b2340ba5359c13359cca
Teeksha <teekshasharma364@gmail.com>
2026-03-29 12:32:35 +0530

    Implement password strength checker

--- tools/password_checker.py
+++ tools/password_checker.py
@@ -1,1 +1,22 @@
 
+import re
+
+password = input("Enter password: ")
+
+if len(password) < 12:
+    print("Weak: Minimum 12 characters required")
+
+elif not re.search("[a-z]", password):
+    print("Weak: Add lowercase letters")
+
+elif not re.search("[A-Z]", password):
+    print("Weak: Add uppercase letters")
+
+elif not re.search("[0-9]", password):
+    print("Weak: Add numbers")
+
+elif not re.search("[@#$%^&+=]", password):
+    print("Weak: Add special characters")
+
+else:
+    print("Strong Password")
